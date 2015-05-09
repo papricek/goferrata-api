d7bca7d240be80f68249556395b6de7c2be75c47
Patrik Jira <patrikjira@gmail.com>
2015-05-09 15:45:37 +0200

    chore: dont use server specific bin

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,7 @@ set :pty, true
 set :linked_files, fetch(:linked_files, []).push('config/secrets.yml', 'db/production.sqlite3')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
