53b3a0ef5bcfac9a1844425440c5dee64b48ed4e
Patrik Jira <patrikjira@gmail.com>
2015-05-10 14:30:32 +0200

    chore: upgrade ruby used on server

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :repo_url, "git@github.com:papricek/#{fetch(:application)}.git"
 # Default value for :scm is :git
 set :scm, :git
 set :rbenv_type, :system
-set :rbenv_ruby, '2.1.4'
+set :rbenv_ruby, '2.2.1'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 
