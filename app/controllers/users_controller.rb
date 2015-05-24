a47157387713dae59e1bec47ac9549e94b4b4908
Patrik Jira <patrikjira@gmail.com>
2015-05-24 10:48:37 +0200

    refactor: share errors template

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,26 @@ class UsersController < ApplicationController
 
   def login
     @user = User.create_from_authorization_code(params[:authorization_code])
-    status = @user.errors.present? ? 400 : 200
-    render "show", status: status
+    if @user.errors.blank?
+      render "show", status: 200
+    else
+      @errors = formatted_errors(@user)
+      render "errors", status: 400
+    end
   rescue User::FacebookProvider::AuthenticationError
     @errors = [{ status: 401, title: "Oooops, man, facebook doesn't care about you." }]
     render "errors", status: 401
+  end
+
+  private
+
+  def formatted_errors(user)
+    user.errors.messages.map do |column, errors|
+      {
+        title: "#{column.to_s.humanize} #{errors.join(", ")}",
+        status: 400
+      }
+    end
   end
 
 end
--- app/views/users/show.jbuilder
+++ app/views/users/show.jbuilder
@@ -1,14 +1,7 @@
-if @user.errors.blank?
-  json.user do
-    json.id         @user.id
-    json.first_name @user.first_name
-    json.last_name  @user.last_name
-    json.email      @user.email
-    json.token      @user.token
-  end
-else
-  json.errors @user.errors.messages do |column, errors|
-    json.title "#{column.to_s.humanize} #{errors.join(", ")}"
-    json.status 400
-  end
+json.user do
+  json.id         @user.id
+  json.first_name @user.first_name
+  json.last_name  @user.last_name
+  json.email      @user.email
+  json.token      @user.token
 end
